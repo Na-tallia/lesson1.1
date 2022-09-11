650faab29d62574206c2cad5a284b1ff640ff739
Na-tallia <mlz.kng@gmail.com>
2022-09-11 19:29:23 +0300

    create2 lesson1.1

--- lesson1.1.py
+++ lesson1.1.py
@@ -1,6 +1,8 @@
-str1 = "hello"
-str2 = "123"
-chislo1 = int(str2)
-print(type(chislo1))
-print(type(str1))
-
+str1 = "translater"
+print(str1[0:11])
+print(str1[0:11:2])
+print(str1[1:11:2])
+print(str1[-1:-11:-1])
+print(str1[6:11])
+print(str1[0:6])
+print(str1[-2:-10:-1])
