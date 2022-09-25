3b1d619a2c9adeb9d87ec5e3204500825c1b07be
Na-tallia <mlz.kng@gmail.com>
2022-09-25 18:44:39 +0300

    dz praktika posle 11 yroka po chislam, indecsam, uslovnie operatori (dopolneno)

--- lesson1.1.py
+++ lesson1.1.py
@@ -1,20 +1,23 @@
 a=input("Введите дату рождения 1го человека в формате дд.мм.гггг ")
 b=input("Введите дату рождения 2го человека дд.мм.гггг ")
-if int(a[6:10])>int(b[6:10]):
-    print("Старше второй человек")
-if int(a[6:10])<int(b[6:10]):
-    print("Старше первый человек")
-if int(a[6:10]) == int(b[6:10]):
-    if int(a[3])>int(b[3]) and int(a[4])>int(b[4]):
+if 0<=int(a[0])<=3 and 0<=int(a[1])<=9 and 0<=int(a[3])<=1 and 0<=int(a[4])<=9 and 0<=int(b[0])<=3 and 0<=int(b[1])<=9 and 0<=int(b[3])<=1 and 0<=int(b[4])<=9 and a[2]!="." and a[5]!=".":
+    if int(a[6:10])>int(b[6:10]):
         print("Старше второй человек")
-    if int(a[3])<int(b[3]):
+    if int(a[6:10])<int(b[6:10]):
         print("Старше первый человек")
-    if int(a[3])==int(b[3]) and int(a[4])>int(b[4]):
-        print("Старше второй человек")
-    if int(a[3])==int(b[3]) and int(a[4])<int(b[4]):
-        print("Старше первый человек")
-    if int(a[3])==int(b[3]) and int(a[4])==int(b[4]):
-        print("Возраст одинаковый")
+    if int(a[6:10]) == int(b[6:10]):
+        if int(a[3])>int(b[3]) and int(a[4])>int(b[4]):
+            print("Старше второй человек")
+        if int(a[3])<int(b[3]):
+            print("Старше первый человек")
+        if int(a[3])==int(b[3]) and int(a[4])>int(b[4]):
+            print("Старше второй человек")
+        if int(a[3])==int(b[3]) and int(a[4])<int(b[4]):
+            print("Старше первый человек")
+        if int(a[3])==int(b[3]) and int(a[4])==int(b[4]):
+            print("Возраст одинаковый")
+else:
+    print("Неправильно введены даты")
 
 
 
