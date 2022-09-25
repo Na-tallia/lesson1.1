416f143b005a205b42fd668bf9e72050c982d496
Na-tallia <mlz.kng@gmail.com>
2022-09-25 15:24:53 +0300

    dz lesson N10 part2 (in file lesson1.1)

--- lesson1.1.py
+++ lesson1.1.py
@@ -1,13 +1,20 @@
-try:
-    a = input("Введите год рождения 4 цифры ")
-    b = input("Введите текущий год 4 цифры ")
-    c = int(b) - int(a)
-except TypeError:
-    print("Вводите только цифры")
-else:
-    if b >= a :
-        print("Количество полных лет составляет",c , "лет", "/" , "года")
-    else:
-        print("Неправильно введен год рождения")
-finally:
-    print("СПАСИБО")
+a = input ("Введите сколько вам лет ")
+if 0<=int(a)<100:
+    if int(a)==0:
+        print("Мне", a ,"лет")
+    if 2<=int(str(a))<5:
+        print ("Мне" , a , "года")
+    if 5 <=int(a)<= 10:
+        print("Мне", a, "лет")
+    if 2<=int(str(a)[0])<10 and int(str(a)[1])==1:
+        print ("Мне" , a , "год")
+    if 2<=int(str(a)[0])<10 and 2<=int(str(a)[1])<=4:
+        print("Мне" , a , "года")
+    if 1<=int(str(a)[0])<=9 and int(str(a)[1])==0:
+        print("Мне", a , "лет")
+    if 95<=int(str(a))<=99:
+        print("Мне", a , "лет")
+    if int(a)==1:
+        print ("Мне" , a , "год")
+else :
+    print ("Неправильно введен возраст")
