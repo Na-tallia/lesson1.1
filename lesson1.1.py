1db658b83f449a6c142246f2ba72efeabd18a07c
Na-tallia <mlz.kng@gmail.com>
2022-09-26 21:00:03 +0300

    dz yrok 10 chast' 2 ispravleno

--- lesson1.1.py
+++ lesson1.1.py
@@ -1,6 +1,20 @@
-a=input("Введите трехзначное число ")
-if 100<=int(a)<=999:
-    print("Сумма цифр числа равна " + str(int(a[0])+int(a[1])+int(a[2])))
-    print("Произведение цифр числа равно " + str(int(a[0])*int(a[1])*int(a[2])))
-else:
-    print("Вы ввели неверное число")
+a = input ("Введите сколько вам лет ")
+if 0<=int(a)<100:
+    if int(a)==0:
+        print("Мне", a ,"лет")
+    elif 2<=int(str(a))<5:
+        print ("Мне" , a , "года")
+    elif 5 <=int(a)<= 10:
+        print("Мне", a, "лет")
+    elif 2<=int(str(a)[0])<10 and int(str(a)[1])==1:
+        print ("Мне" , a , "год")
+    elif 2<=int(str(a)[0])<10 and 2<=int(str(a)[1])<=4:
+        print("Мне" , a , "года")
+    elif 1<=int(str(a)[0])<=9 and int(str(a)[1])==0:
+        print("Мне", a , "лет")
+    elif 95<=int(str(a))<=99:
+        print("Мне", a , "лет")
+    elif int(a)==1:
+        print ("Мне" , a , "год")
+else :
+    print ("Неправильно введен возраст")
