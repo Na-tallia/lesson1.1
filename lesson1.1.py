9ad6955da1c51b3e4fd8a032b3be3612822b81b3
Na-tallia <mlz.kng@gmail.com>
2022-09-26 21:10:23 +0300

    praktika chast' 2 (ne po operatoram,a po ciklu for)

--- lesson1.1.py
+++ lesson1.1.py
@@ -1,20 +1,5 @@
-a = input ("Введите сколько вам лет ")
-if 0<=int(a)<100:
-    if int(a)==0:
-        print("Мне", a ,"лет")
-    elif 2<=int(str(a))<5:
-        print ("Мне" , a , "года")
-    elif 5 <=int(a)<= 10:
-        print("Мне", a, "лет")
-    elif 2<=int(str(a)[0])<10 and int(str(a)[1])==1:
-        print ("Мне" , a , "год")
-    elif 2<=int(str(a)[0])<10 and 2<=int(str(a)[1])<=4:
-        print("Мне" , a , "года")
-    elif 1<=int(str(a)[0])<=9 and int(str(a)[1])==0:
-        print("Мне", a , "лет")
-    elif 95<=int(str(a))<=99:
-        print("Мне", a , "лет")
-    elif int(a)==1:
-        print ("Мне" , a , "год")
-else :
-    print ("Неправильно введен возраст")
+a = int(input ("Введите минимальное число "))
+b=int(input("Введите максимальное число "))
+c=int(input("Введите шаг "))
+for i in range(a,b,c):
+    print(i)
