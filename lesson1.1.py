d0885668cf44ff1cd1b2263c8ff5637a5aed7ae8
Na-tallia <mlz.kng@gmail.com>
2022-10-23 15:48:49 +0300

    практика

--- lesson1.1.py
+++ lesson1.1.py
@@ -1,6 +1,25 @@
-a=input("Введите пятизначное число ")
-max=0
-for i in range(0,5):
-    if int(a[i])>max:
-        max=int(a[i])
-print(max)
+a=int(input("Введите наименьшее число "))
+b=int(input("Введите максимальное число "))
+sum=0
+for i in range(a+1,b):
+    sum=+i
+    print(sum)
+    sum=+i
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
