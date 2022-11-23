aaea1b86ae0be803191e9edb1fcb1ca379e0d1e2
Na-tallia <mlz.kng@gmail.com>
2022-11-23 22:46:42 +0300

    словари

--- lesson1.1.py
+++ lesson1.1.py
@@ -1,12 +1,29 @@
-txt = input("Введите текст, разделяя предложения точками. Другие знаки в предложениях не используйте ").split()
+txt = int(input("Введите количество строк "))
+b=""
+c=[]
 dict={}
-for i in txt:
-    if dict.get(i.lower()) == None:
-        dict[i.lower()] = 1
+for i in range(txt):
+    a=input("Введите строку текста ")
+    b+=" " + a
+c=b.split()
+for j in c:
+    if dict.get(j.lower()) == None:
+        dict[j.lower()]=1
     else:
-        dict[i.lower()]+=1
-for k,v in dict.items():
-    print("Слово", k, "-", v, "раз")
+        dict[j.lower()]+=1
+key1=None
+value1=0
+dict1={}
+for key,value in dict.items():
+    if key!=key1 and value>=value1:
+        key1=key
+        value1=value
+        dict1[key1]=value1
+print(sorted(dict1.items())[0])
+
+
+
+
 
 
 
